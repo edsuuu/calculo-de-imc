6a0e47a6a29ae7afbe5fd433a9d89eea0b4fd201
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-07-25 12:16:58 -0300

    Corrigindo Formatação

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -42,15 +42,15 @@ form.addEventListener('submit', function(event) {
 });
 
 function getNivelImc (imc) {
-    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso','Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']; // a cada array é contado de 0 a 5 separado cada um por , 
-  
+    // a cada array é contado de 0 a 5 separado cada um por ,
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso','Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
     if (imc >= 39.9) return nivel[5];
     if (imc >= 34.9) return nivel[4];
     if (imc >= 29.9) return nivel[3];
     if (imc >= 24.9) return nivel[2];
     if (imc >= 18.5) return nivel[1];
     if (imc < 18.5) return nivel[0];
-  }
+}
 
 function getImc (peso, altura) {
     const imc = peso / altura ** 2;
